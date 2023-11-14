b841dace162eb1e3be1bd6bdc5bd6db1df24e996
Glitch (glitch-hello-website) <none>
2023-11-14 09:12:49 +0000

    🛀🚈 Checkpoint ./session2.js:57911820/887 ./session2.html:57911820/1

--- session2.js
+++ session2.js
@@ -1,1 +1,35 @@
-var
+var name,mothername,fathername,emailid,moblienumber;
+function validation(){
+  readform();
+  
+  if(name ==""){
+    alert("please enter your name");
+    name.focus();
+  }
+  if(mothername ==""){
+    alert("please enter your mothername");
+    mothername.focus();
+  }
+  if(fathername==""){
+    alert("please enter your fathername ");
+    fathername.focus();
+  }
+  if(emailid ==""){
+    alert("please enter your emailid")
+    emailid.focus();
+    
+  }
+  if(moblienumber==""){
+    alert("please enter your moblienumber")
+  moblienumber.focus();
+    
+  }
+  console.log(name,mothername,fathername,emailid,moblienumber);
+  
+  
+}
+function readform(){
+  name = document.getElementById("yname").value;
+  mothername= document.getElementById("mname").value;
+  fathername= document.getElement
+}
