6bd1315aa7d0746dc176f26a37e9ea986cfe5901
Glitch (glitch-hello-website) <none>
2023-11-15 05:49:32 +0000

    🍮🐋 Checkpoint ./session2.html:57911820/5739 ./.glitch-assets:57911820/484 ./task2.html:57911820/164 ./task2fb.js:57911820/457

---
+++ task2fb.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCzG4YVcSF9gjLeuzWl0Mt0jUSPfyxg9nA",
+    authDomain: "project4-5fcde.firebaseapp.com",
+    databaseURL: "https://project4-5fcde-default-rtdb.firebaseio.com",
+    projectId: "project4-5fcde",
+    storageBucket: "project4-5fcde.appspot.com",
+    messagingSenderId: "104517380446",
+    appId: "1:104517380446:web:ae08c67fb7068b2ee187cf",
+    measurementId: "G-RE3PS6QQGZ"
+  };
+firebase.initializeApp(firebaseConfig);
