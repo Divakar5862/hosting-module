b5faf8a642d6b43c3fef79dec0fae5cc6ffb3ef1
Glitch (glitch-hello-website) <none>
2023-11-21 10:38:28 +0000

    🚤💇 Checkpoint ./bmi.js:57911820/608

--- bmi.js
+++ bmi.js
@@ -6,4 +6,20 @@ function calculatebmi(){
   kg=document.getElementById("weight");
   
 }
-if(kg.value==""||ft.va)
+if(kg.value==""||ft.value==""){
+  alert("please enter your weight and feet");
+  
+}
+else{
+  var meter=ft.value*0.3048;
+  var height=meter*meter;
+  var bmi=kg.value/height;
+  console.log ("bmi value="+bmi);
+  if(bmi<18.5){
+    console.log("the user is under weight eat healthy food ");
+    result.innerHTML="<h2>"+bmi+"</h2>";
+    tips.innerHTML="<h2>"+"the user is under weight eat healthy food"+"</h2>";
+    result1=result.innerHTML
+  }
+  
+}
