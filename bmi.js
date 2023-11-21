e8c5685fca76eb0b7f2b03f489f00fc7df25ce51
Glitch (glitch-hello-website) <none>
2023-11-21 10:49:53 +0000

    🌑🏮 Checkpoint ./bmi.js:57911820/1850

--- bmi.js
+++ bmi.js
@@ -19,7 +19,42 @@ else{
     console.log("the user is under weight eat healthy food ");
     result.innerHTML="<h2>"+bmi+"</h2>";
     tips.innerHTML="<h2>"+"the user is under weight eat healthy food"+"</h2>";
-    result1=result.innerHTML
+    result1=result.innerHTML;
+    tips1=tips.innerHTML;
   }
+  else if(bmi>=18.5&&bmi<25){
+     console.log("the user is normal please maintained same diet ");
+    result.innerHTML="<h2>"+bmi+"</h2>";
+    tips.innerHTML="<h2>"+"the user is normal please maintained same diet"+"</h2>";
+    result1=result.innerHTML;
+    tips1=tips.innerHTML;
+    
+  }
+  else if(bmi>=25&&bmi<30){
+     console.log("the user is overweight please maintained diet ");
+    result.innerHTML="<h2>"+bmi+"</h2>";
+    tips.innerHTML="<h2>"+"the user is overwight please maintained deit"+"</h2>";
+    result1=result.innerHTML;
+    tips1=tips.innerHTML;
+    
+    
+  }
+  else if(bmi>=30&&bmi<35){
+     console.log("the user is obese please check with  your doctor ");
+    result.innerHTML="<h2>"+bmi+"</h2>";
+    tips.innerHTML="<h2>"+"the user is obese please check with your doctor"+"</h2>";
+    result1=result.innerHTML;
+    tips1=tips.innerHTML;
+  }
+  else if(bmi>=35){
+     console.log("the user is facing obese kindly check with your doctor and take immediate action ");
+    result.innerHTML="<h2>"+bmi+"</h2>";
+    tips.innerHTML="<h2>"+"the user is the user is facing obese kindly check with your doctor and take immediate action"+"</h2>";
+    result1=result.innerHTML;
+    tips1=tips.innerHTML;
+    
+  }
+  el
+  
   
 }
