6b7b8849476819225dbaf34281cfec390f24a31f
Glitch (glitch-hello-website) <none>
2023-11-21 10:27:08 +0000

    🍵🐛 Checkpoint ./bmi.js:57911820/394 ./bmi.html:57911820/37

--- bmi.js
+++ bmi.js
@@ -1,1 +1,9 @@
-v
+var result,tips,ft,kg,result1,tips1;
+function calculatebmi(){
+  result=document.getElementById("bmiresult");
+  tips=document.getElementById("bmiresult1");
+  ft=document.getElementById("feet");
+  kg=document.getElementById("weight");
+  
+}
+if(kg.value==""||ft.va)
