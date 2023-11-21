289afc3312cc597f25a4761653db22875923001c
Glitch (glitch-hello-website) <none>
2023-11-21 10:03:59 +0000

    🌓🍚 Checkpoint ./task3.html:57911820/7 ./task3fb.js:57911820/456

---
+++ task3fb.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCLZz-BAECWmmYMFCIuy0zruOpnwwqM09w",
+    authDomain: "project6-b20e8.firebaseapp.com",
+    databaseURL: "https://project6-b20e8-default-rtdb.firebaseio.com",
+    projectId: "project6-b20e8",
+    storageBucket: "project6-b20e8.appspot.com",
+    messagingSenderId: "726931872633",
+    appId: "1:726931872633:web:0d35e69b17f8195c7a2da8",
+    measurementId: "G-JK2MLC2FYQ"
+  };
+firebase.initializeApp(firebaseConfig);
