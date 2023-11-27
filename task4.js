d7fa68d22c609ed9efac95ac40845a3281eddb60
Glitch (glitch-hello-website) <none>
2023-11-27 09:32:51 +0000

    🚍📟 Checkpoint ./task4.js:57911820/5901 ./task4.html:57911820/12 ./task4fb.js:57911820/1289 ./task4.css:57911820/16076

--- task4.js
+++ task4.js
@@ -5,78 +5,77 @@ function calculatebmi() {
   foot = document.getElementById("bmifeet");
   kg = document.getElementById("weight");
 
-if (kg.value == "" || foot.value == "") {
-  alert("please enter your weight and feet");
-} else {
-  var meter = foot.value * 0.3048;
-  var height = meter * meter;
-  var bmi = kg.value / height;
-  console.log("bmi value=" + bmi);
-  if (bmi < 18.5) {
-    console.log("the user is under weight eat healthy food ");
-    result.innerHTML = "<h2>" + bmi + "</h2>";
-    dips.innerHTML =
-      "<h2>" + "the user is under weight eat healthy food" + "</h2>";
-    result1 = result.innerHTML;
-    tips1 = dips.innerHTML;
-  } else if (bmi >= 18.5 && bmi < 25) {
-    console.log("the user is normal please maintained same diet ");
-    result.innerHTML = "<h2>" + bmi + "</h2>";
-    dips.innerHTML =
-      "<h2>" + "the user is normal please maintained same diet" + "</h2>";
-    result1 = result.innerHTML;
-    tips1 = dips.innerHTML;
-  } else if (bmi >= 25 && bmi < 30) {
-    console.log("the user is overweight please maintained diet ");
-    result.innerHTML = "<h2>" + bmi + "</h2>";
-    dips.innerHTML =
-      "<h2>" + "the user is overwight please maintained deit" + "</h2>";
-    result1 = result.innerHTML;
-    tips1 = dips.innerHTML;
-  } else if (bmi >= 30 && bmi < 35) {
-    console.log("the user is obese please check with  your doctor ");
-    result.innerHTML = "<h2>" + bmi + "</h2>";
-    dips.innerHTML =
-      "<h2>" + "the user is obese please check with your doctor" + "</h2>";
-    result1 = result.innerHTML;
-    tips1 = dips.innerHTML;
-  } else if (bmi >= 35) {
-    console.log(
-      "the user is facing obese kindly check with your doctor and take immediate action "
-    );
-    result.innerHTML = "<h2>" + bmi + "</h2>";
-    dips.innerHTML =
-      "<h2>" +
-      "the user is the user is facing obese kindly check with your doctor and take immediate action" +
-      "</h2>";
-    result1 = result.innerHTML;
-    tips1 = dips.innerHTML;
+  if (kg.value == "" || foot.value == "") {
+    alert("please enter your weight and feet");
   } else {
-    console.log("check your input");
-    result.innerHTML =
-      "<h2>" +
-      "the user is the user is facing obese kindly check with your doctor and take immediate action" +
-      "</h2>";
+    var meter = foot.value * 0.3048;
+    var height = meter * meter;
+    var bmi = kg.value / height;
+    console.log("bmi value=" + bmi);
+    if (bmi < 18.5) {
+      console.log("the user is under weight eat healthy food ");
+      result.innerHTML = "<h2>" + bmi + "</h2>";
+      dips.innerHTML =
+        "<h2>" + "the user is under weight eat healthy food" + "</h2>";
+      result1 = result.innerHTML;
+      tips1 = dips.innerHTML;
+    } else if (bmi >= 18.5 && bmi < 25) {
+      console.log("the user is normal please maintained same diet ");
+      result.innerHTML = "<h2>" + bmi + "</h2>";
+      dips.innerHTML =
+        "<h2>" + "the user is normal please maintained same diet" + "</h2>";
+      result1 = result.innerHTML;
+      tips1 = dips.innerHTML;
+    } else if (bmi >= 25 && bmi < 30) {
+      console.log("the user is overweight please maintained diet ");
+      result.innerHTML = "<h2>" + bmi + "</h2>";
+      dips.innerHTML =
+        "<h2>" + "the user is overwight please maintained deit" + "</h2>";
+      result1 = result.innerHTML;
+      tips1 = dips.innerHTML;
+    } else if (bmi >= 30 && bmi < 35) {
+      console.log("the user is obese please check with  your doctor ");
+      result.innerHTML = "<h2>" + bmi + "</h2>";
+      dips.innerHTML =
+        "<h2>" + "the user is obese please check with your doctor" + "</h2>";
+      result1 = result.innerHTML;
+      tips1 = dips.innerHTML;
+    } else if (bmi >= 35) {
+      console.log(
+        "the user is facing obese kindly check with your doctor and take immediate action "
+      );
+      result.innerHTML = "<h2>" + bmi + "</h2>";
+      dips.innerHTML =
+        "<h2>" +
+        "the user is the user is facing obese kindly check with your doctor and take immediate action" +
+        "</h2>";
+      result1 = result.innerHTML;
+      tips1 = dips.innerHTML;
+    } else {
+      console.log("check your input");
+      result.innerHTML =
+        "<h2>" +
+        "the user is the user is facing obese kindly check with your doctor and take immediate action" +
+        "</h2>";
+    }
   }
 }
+function readForm() {
+  var result = document.getElementById("bmiresult");
+  var tips = document.getElementById("bmiresult1");
+  var foot = document.getElementById("bmifeet");
+  var kg = document.getElementById("weight");
 }
-function readForm(){
-  var result=document.getElementById("bmiresult");
-  var tips=document.getElementById("bmiresult1");
-  var foot=document.getElementById("bmifeet");
-  var kg=document.getElementById("weight");
-  
-}
-document.getElementById("submit").onclick=function(){
-   calculatebmi();
+document.getElementById("submit").onclick = function () {
+  calculatebmi();
   readForm();
- RESULT:result1,
-    DIPS:tips1,
-    FOOT:feet,
-    KG:kg,
-    
-  })
+  firebase.database().ref("submitted/").set({
+    RESULT: result1,
+    DIPS: tips1,
+    FOOT: foot,
+    KG: kg,
+  });
   alert("data submitted");
-  feet=document.getElementById("bmifeet").value="";
-  kg=document.getElementById("weight").value="";
-}
+  foot = document.getElementById("bmifeet").value = "";
+  kg = document.getElementById("weight").value = "";
+};
---
+++ task4fb.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAo_AsbGuNlhokKIpWPJTlHzxmCMnNikFU",
+    authDomain: "project2-26ebb.firebaseapp.com",
+    databaseURL: "https://project2-26ebb-default-rtdb.firebaseio.com",
+    projectId: "project2-26ebb",
+    storageBucket: "project2-26ebb.appspot.com",
+    messagingSenderId: "683738241995",
+    appId: "1:683738241995:web:d88e8cad04d13f8c0c344e",
+    measurementId: "G-VX7D7E038N"
+  };
+firebase.initializeApp(firebaseConfig);
